5e53b80ad12e2fee1f6936a1cb4f21fcf9212f14
QA-Lady <qa.lady5@gmail.com>
2017-03-16 20:46:31 -0400

    homework: 2 with constructor that accepts two parameters

--- sandbox/src/main/java/prg/training/CheckPoint.java
+++ sandbox/src/main/java/prg/training/CheckPoint.java
@@ -6,8 +6,9 @@ package prg.training;
 public class CheckPoint {
 
     public static void main(String[] args) {
-        Point p = new Point(-2.7, 2.4, 8.5, 0.8);
-        System.out.println("The distance between points x" + p.x1 + ", y1" + p.y1 + " and x2" + p.x2 + ", y2" + p.y2 + " is eaqual to " + p.distance());
+        Point a = new Point(-2.7, 2.4);
+        Point b = new Point(8.5, 0.8);
+        System.out.println("The distance between points A: " + a + " and B: " + b + " is eaqual to " + a.distance(b));
 
     }
 }
