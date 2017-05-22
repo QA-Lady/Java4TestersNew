35c5e8d98910b623c7fa94c5b4c449014e44ac50
QA-Lady <qa.lady5@gmail.com>
2017-05-21 23:30:42 -0400

    Added github-sample module: - added all required libs and Github test that prints all commits from this project

---
+++ github-sample/src/test/java/prg/training/github/GithubTests.java
@@ -0,0 +1,19 @@
+package prg.training.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("b4a8615e2fae228a91cf6a56139eea875ec08df1");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("QA-Lady", "Java4TestersNew")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
