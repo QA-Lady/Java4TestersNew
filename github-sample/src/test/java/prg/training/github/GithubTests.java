0ea16c93e7e0ea2b7acdac5374f8d417f4e3b074
QA-Lady <qa.lady5@gmail.com>
2017-05-22 00:35:50 -0400

    added useTestNG() to sandbox build file

--- github-sample/src/test/java/prg/training/github/GithubTests.java
+++ github-sample/src/test/java/prg/training/github/GithubTests.java
@@ -10,7 +10,7 @@ public class GithubTests {
 
     @Test
     public void testCommits() throws IOException {
-        Github github = new RtGithub("b4a8615e2fae228a91cf6a56139eea875ec08df1");
+        Github github = new RtGithub("7bbcbec0f36dc33b69380e2e04043c47b0f282f4");
         RepoCommits commits = github.repos().get(new Coordinates.Simple("QA-Lady", "Java4TestersNew")).commits();
         for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
             System.out.println(new RepoCommit.Smart(commit).message());
